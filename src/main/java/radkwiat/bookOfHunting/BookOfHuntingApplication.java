d8fe2c8fbba6b21cbc42ace5d606727377c4fd2d
Radosław Kwiatkowski <35296705+radkwiat84@users.noreply.github.com>
2018-05-30 13:38:34 +0200

    adding adnotation @ComponentScan

--- src/main/java/radkwiat/bookOfHunting/BookOfHuntingApplication.java
+++ src/main/java/radkwiat/bookOfHunting/BookOfHuntingApplication.java
@@ -2,8 +2,10 @@ package radkwiat.bookOfHunting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class BookOfHuntingApplication {
 
 	public static void main(String[] args) {

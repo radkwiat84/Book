b9d1fa15def7c2d61930bd4e56acc5d11b44b8e4
Radosław Kwiatkowski <35296705+radkwiat84@users.noreply.github.com>
2018-05-30 12:56:37 +0200

    Initial commit

---
+++ src/main/java/radkwiat/bookOfHunting/BookOfHuntingApplication.java
@@ -0,0 +1,12 @@
+package radkwiat.bookOfHunting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookOfHuntingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookOfHuntingApplication.class, args);
+	}
+}

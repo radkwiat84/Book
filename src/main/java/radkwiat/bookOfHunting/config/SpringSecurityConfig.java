21b58c25dfa2638e6468636e91df2016954a2ac7
Radosław Kwiatkowski <35296705+radkwiat84@users.noreply.github.com>
2018-05-31 23:21:21 +0200

    created entityies: Role and User

--- src/main/java/radkwiat/bookOfHunting/config/SpringSecurityConfig.java
+++ src/main/java/radkwiat/bookOfHunting/config/SpringSecurityConfig.java
@@ -21,28 +21,14 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	private BCryptPasswordEncoder bcp;
 
 	@Autowired
-	// do autoryzaji, nawiązania połączeń, wykonywania skryptów
 	private DataSource ds;
 
-	/*
-	 * Przy pomocy tej zmiennej będę się posługiwać zapytaniem o użytkownika
-	 * 
-	 * w adnotacji value mapujemy to co jest w application.properties (czyli zostaje
-	 * to pobrane z appli*.prop* i zmapowane na tą zmienną
-	 */
 	@Value("${spring.queries.users-query}")
 	private String usersQuery;
 
-	/*
-	 * Tą zmienną posłużę się do zapytania o rolę.
-	 */
 	@Value("${spring.queries.roles-query}")
 	private String rolesQuery;
 
-	/*
-	 * poniżej pierwsza metoda konfiguracyjna. Autentykacja poprzez zapytanie
-	 * (usersQuery), poprzez rolę użytkownika itd.
-	 */
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -52,26 +38,18 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity httpSecur) throws Exception {
-		// nie trzeba się logować na stronie głównej
 		httpSecur.authorizeRequests().antMatchers("/").permitAll().antMatchers("/login").permitAll()
 				.antMatchers("/register").permitAll()
-				// musi być autentykacja typu ADMIN (rola Admin)
 				.antMatchers("/admin/**").hasAnyAuthority("ADMIN").anyRequest().authenticated().and().csrf().disable()
-				// jeżeli logowanie jest błędne to przekazywany jest ten url
 				.formLogin().loginPage("/login").failureUrl("/login?error=true")
-				// do logowania chwycimy email i hasło = > i jak bangla to na stronę główną
-				// jak nie zabangla to na stronę login z parametre error=true.
 				.defaultSuccessUrl("/").usernameParameter("email").passwordParameter("password")
-				// po wylogowaniu nowy matcher logout
 				.and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/")
-				// kiedy chcemy się pod inne url dostać niż te zdefiniowane to jest denied
 				.and().exceptionHandling().accessDeniedPage("/denied");
 
 	}
-
+							
 	@Override
 	public void configure(WebSecurity webSecur) throws Exception {
-		//SpringSecurity ignoruje wszystko co jest odczytywane z tych katalogów
 		webSecur.ignoring().antMatchers("/resources/**", "/statics/**", "/css/**", "/js/**", "/images/**");
 	}
 

18ff2789ff5eb6ad0f1b3192c4fb8bb8e1f38fbb
Radosław Kwiatkowski <35296705+radkwiat84@users.noreply.github.com>
2018-06-04 07:23:35 +0200

    Revert "Revert "first message.properties""
    This reverts commit e97f5dffaa52ad2f6808ba5033ddd85862692cd4.

--- src/main/java/radkwiat/bookOfHunting/config/SpringSecurityConfig.java
+++ src/main/java/radkwiat/bookOfHunting/config/SpringSecurityConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web .util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -29,7 +29,6 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Value("${spring.queries.roles-query}")
 	private String rolesQuery;
 
-
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.jdbcAuthentication().usersByUsernameQuery(usersQuery).authoritiesByUsernameQuery(rolesQuery).dataSource(ds)
@@ -38,16 +37,24 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity httpSecur) throws Exception {
-		httpSecur.authorizeRequests().antMatchers("/").permitAll().antMatchers("/login").permitAll()
+		httpSecur.authorizeRequests()
+		.antMatchers("/").permitAll()
+		.antMatchers("/login").permitAll()
 				.antMatchers("/register").permitAll()
-				.antMatchers("/admin/**").hasAnyAuthority("ADMIN").anyRequest().authenticated().and().csrf().disable()
-				.formLogin().loginPage("/login").failureUrl("/login?error=true")
-				.defaultSuccessUrl("/").usernameParameter("email").passwordParameter("password")
-				.and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/")
+				.antMatchers("/adduser").permitAll()
+				.antMatchers("/admin/**").hasAnyAuthority("ADMIN")
+				.anyRequest().authenticated()
+				.and().csrf().disable()
+				.formLogin().loginPage("/login")
+				.failureUrl("/login?error=true")
+				.defaultSuccessUrl("/").usernameParameter("email")
+				.passwordParameter("password")
+				.and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+				.logoutSuccessUrl("/")
 				.and().exceptionHandling().accessDeniedPage("/denied");
 
 	}
-							
+
 	@Override
 	public void configure(WebSecurity webSecur) throws Exception {
 		webSecur.ignoring().antMatchers("/resources/**", "/statics/**", "/css/**", "/js/**", "/images/**");

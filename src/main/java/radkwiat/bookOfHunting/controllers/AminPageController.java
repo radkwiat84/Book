72e1fd824e6774539c7326f6f917374d9ac4ff49
Radosław Kwiatkowski <35296705+radkwiat84@users.noreply.github.com>
2018-06-22 09:15:41 +0200

    added methods => modification users by admins

--- src/main/java/radkwiat/bookOfHunting/controllers/AminPageController.java
+++ src/main/java/radkwiat/bookOfHunting/controllers/AminPageController.java
@@ -1,10 +1,17 @@
 package radkwiat.bookOfHunting.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import radkwiat.bookOfHunting.models.User;
@@ -29,8 +36,10 @@ public class AminPageController {
 		userList = userService.findAll();
 
 		for (User users : userList) {
-			
+
 			int numberOfRole = users.getRoles().iterator().next().getId();
+
+			
 
 			if (numberOfRole == 1) {
 				users.setRolaInt(numberOfRole);
@@ -41,6 +50,73 @@ public class AminPageController {
 		model.addAttribute("userList", userList);
 
 		return "admin/users";
+	}
+
+	@RequestMapping("/admin/edit/{id}")
+	@Secured({ "ROLE_ADMIN" })
+	public String getUserToEdit(@PathVariable("id") int id, Model model) {
+
+		User user = new User();
+		/*
+		 * automatyczne parsowanie w parametrach metody w adnotacji PathVariable
+		 */
+		// int userId = Integer.parseInt(id);
+		user = userService.findUserById(id);
+
+		Map<Integer, String> roleMap = new HashMap<>();
+		roleMap.put(1, "Administrator");
+		roleMap.put(2, "Użytkownik");
+
+		int rola = user.getRoles().iterator().next().getId();
+
+		user.setRolaInt(rola);
+
+		model.addAttribute("roleMap", roleMap);
+		model.addAttribute("user", user);
+
+		return "admin/useredit";
+	}
+
+	@RequestMapping("/admin/update")
+	@Secured({"ROLE_ADMIN"})
+	public String updateUser(Model model, @Valid User user, BindingResult result) {
+
+		String returnPage = null;
+		String rola = null;
+
+		if (user.getRolaInt() == 1) {
+			rola = "ROLE_ADMIN";
+		} else {
+			rola = "ROLE_USER";
+		}
+
+		if (result.hasErrors()) {
+			Map<Integer, String> roleMap = new HashMap<>();
+			roleMap.put(1, "Administrator");
+			roleMap.put(2, "Użytkownik");
+			model.addAttribute("roleMap", roleMap);
+
+			returnPage = "admin/useredit";
+		} else {
+
+			userService.updateUser(rola, user);
+
+			List<User> userList = userService.findAll();
+
+			for (User users : userList) {
+				int numberOfRole = users.getRoles().iterator().next().getId();
+
+				if (numberOfRole == 1) {
+					users.setRolaInt(numberOfRole);
+				} else if (numberOfRole == 2) {
+					users.setRolaInt(numberOfRole);
+				}
+			}
+			model.addAttribute("userList", userList);
+			returnPage = "admin/users";
+		}
+
+		return returnPage;
 	}
 
 }
